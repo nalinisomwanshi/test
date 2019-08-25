75c8da79760adde9011cee716829b45bda88ad09
nalinisomwanshi <nalinisomwanshi@gmail.com>
2019-08-25 12:27:42 +0530

    Created a blog component for list operation

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  title = 'PokeMon';
+  cars = ['Baleno', 'Swift', 'Verna', 'Scorpio', 'Celerio', 'Ciaz', 'City', 'Lamborgini'];
+
+  constructor() {
+    setTimeout(() => {
+      this.title = 'DoreMon'
+    }, 5000);
+  }
+
+  updateText() {
+    this.title = "SpiderMan";
+  }
+}
---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent {
+
+  newBlog: string;
+  blogList: Array<string> = [];
+
+  constructor() { }
+
+  addNewBlog(): void {
+
+    if(!this.newBlog) {
+      return;
+    }
+
+    this.blogList.push(this.newBlog);
+    this.newBlog = '';
+  }
+  deleteNewBlog(i:number): void{
+   // this.blogList.splice();
+   
+   this.blogList.splice(i,1);
+   //array changed re-render list
+   //renderList();
+
+   
+  }
+
+}
